54f45e797928b9b206c4f2485ff513b495e0b1f2
Egor Fedorov <egor.fedorov@codestetic.com>
2025-04-14 16:15:22 +0000

    Настроил

--- src/BlackCandle.API/Program.cs
+++ src/BlackCandle.API/Program.cs
@@ -1,10 +1,16 @@
 using BlackCandle.API.Configuration;
 using BlackCandle.API.Middleware;
+using BlackCandle.Web.Components;
+
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -18,15 +24,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddProjectServices(builder.Configuration);
 builder.Services.AddSwaggerDocumentation();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
 app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAntiforgery();
+
+app.UseAuthorization();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+app.MapRazorPages();
 
 app.UseSwaggerDocumentation();
 
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
